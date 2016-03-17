2af292cc3ee93fd94f19355dc26f5c7cb4c388f4
Michał Łowicki <mlowicki@opera.com>
2016-03-17 23:41:23 +0100

    Prototype of parsing requirements and asking PyPI for details

--- sanestack/main.py
+++ sanestack/main.py
@@ -1,15 +1,31 @@
+from pip.req import parse_requirements
 import logging
+import uuid
 
 from argh import arg, dispatch_commands
+import requests
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 @arg('path', help='Path to check (directory or concrete file)')
 def check(path):
     logger.info('Checking "%s"', path)
+
+    for requirement in parse_requirements(path, session=uuid.uuid1()):
+        url = 'https://pypi.python.org/pypi/%s/json' % requirement.name
+        response = requests.get(url)
+
+        if not response.ok:
+            logging.error('Request to %s failed (%d)', url,
+                          response.status_code)
+            continue
+
+        info = response.json()
+        import json
+        logging.info(json.dumps(info, indent=2))
 
 
 def main():
