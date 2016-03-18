941a615543847dc31baa9ed64b8958bf624cf412
Michał Łowicki <mlowicki@gmail.com>
2016-03-18 18:53:59 +0100

    Sort updates. Introduce  function

--- sanestack/main.py
+++ sanestack/main.py
@@ -61,6 +61,40 @@ def is_update(requirement, version):
     return True
 
 
+def find_updates(requirement, legacy_versions, pre_releases):
+    url = 'https://pypi.python.org/pypi/%s/json' % requirement.name
+    response = requests.get(url)
+
+    if not response.ok:
+        logger.error('Request to %s failed (%d)', url,
+                      response.status_code)
+        return
+
+    info = response.json()
+    updates = []
+
+    for version in info['releases'].keys():
+        try:
+            version = parse(version)
+        except InvalidVersion:
+            logger.error('Cannot parse version: %s', version)
+            continue
+
+        if not legacy_versions and isinstance(version, LegacyVersion):
+            continue
+
+        if not pre_releases and version.is_prerelease:
+            continue
+
+        if is_update(requirement, version):
+            updates.append(version)
+
+    if updates:
+        updates.sort()
+        logger.info('Updates for %s available: %s', requirement.name,
+                    [str(version) for version in updates])
+
+
 @arg('-v', '--verbose', help='Verbose mode')
 @arg('--legacy-versions', help='Show legacy versions')
 @arg('--pre-releases', help='Show pre-releases (alpha, beta etc.)')
@@ -73,35 +107,9 @@ def check(path, pre_releases=False, legacy_versions=False, verbose=False):
 
     for requirement in parse_requirements(path, session=session,
                                           finder=finder):
-        url = 'https://pypi.python.org/pypi/%s/json' % requirement.name
-        response = requests.get(url)
-
-        if not response.ok:
-            logger.error('Request to %s failed (%d)', url,
-                          response.status_code)
-            continue
-
-        info = response.json()
-        updates = []
-
-        for version in info['releases'].keys():
-            try:
-                version = parse(version)
-            except InvalidVersion:
-                logger.error('Cannot parse version: %s', version)
-                continue
-
-            if not legacy_versions and isinstance(version, LegacyVersion):
-                continue
-
-            if not pre_releases and version.is_prerelease:
-                continue
-
-            if is_update(requirement, version):
-                updates.append(str(version))
-
-        if updates:
-            logger.info('Updates for %s available: %s', requirement.name, updates)
+        find_updates(requirement=requirement,
+                     legacy_versions=legacy_versions,
+                     pre_releases=pre_releases)
 
 def main():
     dispatch_commands([check])
