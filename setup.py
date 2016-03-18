d3d9ccd5dbf21d5377a2ea0562fcfe2862b0d6d2
Michał Łowicki <mlowicki@gmail.com>
2016-03-18 20:46:13 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='sanestack',
-      version='0.0.2',
+      version='0.0.3',
       description='Check if your dependencies are up to date.',
       author='Michał Łowicki',
       author_email='mlowicki@gmail.com',
